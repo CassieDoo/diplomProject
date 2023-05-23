637113f86a9fe867f39898348b8a2a6648b16393
CassieDoo <kse8743@yandex.ru>
2023-05-23 19:55:56 +0500

    upd

--- interface.py
+++ interface.py
@@ -1,14 +1,17 @@
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
+from datetime import date
 from config import comunity_token
 from main import *
-from database import add_worksheet
+from database import add_worksheet, find_worksheets
 
 
 class BotInterface:
 
     def __init__(self, token):
         self.bot = vk_api.VkApi(token=token)
+        user_info = tools.get_profile_info()
+        user_id = user_info[0]['id']
 
     def message_send(self, user_id, message=None, attachment=None):
         self.bot.method('messages.send',
@@ -18,7 +21,83 @@ class BotInterface:
                          'attachment': attachment
                          })
 
-    def handler(self):
+    def get_search_city(self, user_id):
+        try:
+            city_id = user_info[0]['city']['id']
+
+        except KeyError:
+            self.message_send(user_id, message='Укажите город для поиска:')
+
+            longpoll = VkLongPoll(self.bot)
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                    city_title = event.text.capitalize()
+
+                    try:
+                        city = tools.ext_api.method('database.getCities',
+                                                    {'q': city_title
+                                                     })
+
+                        city_id = city['items'][0]['id']
+                        break
+
+                    except KeyError:
+                        self.message_send(user_id, message='Город не найден')
+                        return
+
+        return city_id
+
+    def get_search_sex(self, user_id):
+        try:
+            user_sex = user_info[0]['sex']
+            if user_sex == 1:
+                sex = 2
+            elif user_sex == 2:
+                sex = 1
+
+        except KeyError:
+            self.message_send(user_id, message='Укажите пол партнера для поиска:')
+
+            longpoll = VkLongPoll(self.bot)
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                    sex_title = event.text.lower()
+                    if sex_title == 'женский' or 'жен' or 'ж':
+                        sex = 1
+                    elif sex_title == 'мужской' or 'муж' or 'м':
+                        sex = 2
+                    else:
+                        self.message_send(event.user_id, 'пол не определен')
+
+        return sex
+
+    def get_search_age(self, user_id):
+        try:
+            bdate = user_info[0]['bdate'].split('.')
+            born = date(int(bdate[2]), int(bdate[1]), int(bdate[0]))
+            today = date.today()
+            age = today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+        except KeyError:
+            self.message_send(user_id, message='Укажите ваш возраст:')
+
+            longpoll = VkLongPoll(self.bot)
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                    age = event.text
+
+        if (age - 5) >= 18:
+            age_from = age - 5
+        else:
+            age_from = 18
+
+        age_to = age + 5
+
+        search_age = {'age_from': age_from, 'age_to': age_to}
+
+        return search_age
+
+    def handler(self, offset=0):
         longpoll = VkLongPoll(self.bot)
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
@@ -26,13 +105,35 @@ class BotInterface:
                     self.message_send(event.user_id, 'Добрый день')
 
                 elif event.text.lower() == 'поиск':
-                    result = tools.users_search()
-                    self.message_send(event.user_id, f"{result['name']} \n"
-                                                     f"vk.com/id{result['id']}")
-                    add_worksheet(result['id'])
-                    media_list = bot.get_media(result['id'])
-                    for media in media_list:
-                        self.message_send(event.user_id, attachment=media)
+                    city_id = self.get_search_city(user_id)
+                    sex = self.get_search_sex(user_id)
+                    age_from = self.get_search_age(user_id)['age_from']
+                    age_to = self.get_search_age(user_id)['age_to']
+
+                    result = tools.users_search(city_id, sex, age_from, age_to, offset)
+                    self.message_send(event.user_id, 'Анкеты  подобраны, для просморта нажите "далее"')
+
+                elif event.text.lower() == 'далее':
+                    for worksheet in result:
+                        if find_worksheets(worksheet['id']) == 0:
+                            self.message_send(event.user_id, f"{worksheet['name']} \n"
+                                                             f"vk.com/id{worksheet['id']}")
+                            add_worksheet(worksheet['id'])
+                            media_list = bot.get_media(worksheet['id'])
+
+                            for media in media_list:
+                                self.message_send(event.user_id, attachment=media)
+
+                            result.remove(worksheet)
+                            break
+
+                        else:
+                            result.remove(worksheet)
+                            continue
+
+                    if not result:
+                        self.message_send(event.user_id, 'Для поиска новых анкет нажмите "поиск"')
+                        offset += 10
 
                 else:
                     self.message_send(event.user_id, 'неизвестная команда ')
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from datetime import date
 import vk_api
 from vk_api.exceptions import ApiError
 from config import acces_token
 from database import find_worksheets
-
 
 class VkTools:
     def __init__(self, token):
@@ -19,44 +17,14 @@ class VkTools:
 
         return user_info
 
-    @staticmethod
-    def get_search_info(user_info):
+    def users_search(self, city_id, sex, age_from, age_to, offset=None):
 
-        city_id = user_info[0]['city']['id']
-
-        bdate = user_info[0]['bdate'].split('.')
-        born = date(int(bdate[2]), int(bdate[1]), int(bdate[0]))
-        today = date.today()
-        age = today.year - born.year - ((today.month, today.day) < (born.month, born.day))
-
-        if (age - 5) >= 18:
-            age_from = age - 5
-        else:
-            age_from = 18
-
-        age_to = age + 5
-
-        user_sex = user_info[0]['sex']
-        if user_sex == 1:
-            sex = 2
-        elif user_sex == 2:
-            sex = 1
-        else:
-            print('пол не определен')
-            sex = 0
-
-        search_info = {'city_id': city_id, 'age_from': age_from, 'age_to': age_to, 'sex': sex}
-
-        return search_info
-
-    def users_search(self, offset=None):
-        search_info = tools.get_search_info(user_info)
         try:
             profiles = self.ext_api.method('users.search',
-                                           {'city_id': search_info['city_id'],
-                                            'age_from': search_info['age_from'],
-                                            'age_to': search_info['age_to'],
-                                            'sex': search_info['sex'],
+                                           {'city_id': city_id,
+                                            'age_from': age_from,
+                                            'age_to': age_to,
+                                            'sex': sex,
                                             'status': 6,
                                             'fields': 'relation',
                                             'count': 10,
@@ -68,14 +36,15 @@ class VkTools:
 
         profiles = profiles['items']
 
+        result = []
         for profile in profiles:
             try:
                 relation = profile['relation']
 
-                if profile['is_closed'] == False and find_worksheets(profile['id']) == 0 and relation == 6:
-                    result = {'name': profile['first_name'] + ' ' + profile['last_name'],
-                              'id': profile['id']
-                              }
+                if profile['is_closed'] == False and relation == 6 and find_worksheets(profile['id']) == 0:
+                    result.append({'name': profile['first_name'] + ' ' + profile['last_name'],
+                                   'id': profile['id']
+                                   })
 
                 else:
                     continue
